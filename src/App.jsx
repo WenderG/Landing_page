e818fcaf9dc7d09e350701b397ccaa3045cca870
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-03-30 06:58:33 -0300

    :books: docs: Modificações

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import Header from './assets/components/Header'
+import Footer from './assets/components/Footer'
 
 function App() {
  
   return (
     <div className="App">
-      
+      <Header/>
+      <Footer/>
     </div>
   )
 }
